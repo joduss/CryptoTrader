88f36535d8b53094f55d2842dbd64568f685bb00
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-01-19 19:18:15 +0100

    Create proper swift command line program.

--- Trader2/main.swift
+++ Trader2/main.swift
@@ -1,6 +1,3 @@
-import Foundation
-
-//
 //  main.swift
 //  Trader2
 //
@@ -8,113 +5,59 @@ import Foundation
 //
 
 import Foundation
-
+import ArgumentParser
 import Cocoa
 import os
 
-sourcePrint("Started Trader.")
+let arguments = CommandLine.arguments
 
-
-
-
-
-
-let fileUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("prices-read3.json")
-let data = try Data(contentsOf: fileUrl)
-let prices = try JSONDecoder().decode([PriceRecord].self, from: data)
-
-
-print("Running trader with simulation api.")
-let api = SimulatedTradingPlatform(prices: prices)
-let trader = Trader(api: api)
-api.startSimulation(completed: {
-    print("Total profits: \(trader.profits)")
-})
-
-//print("Running trader with kraken api.")
-//let api = Kraken()
-//let trader = PriceRecorder(api: api)
-
-//sourcePrint("Running trader with Binance api.")
-//let api = Binance()
-//let trader = PriceRecorder(api: api)
-
-
-
-RunLoop.main.run()
-
-
-
-class Response: Codable {
-
-    let response: String
-
-    var price: Double? = nil
-
-    private var responseData: Data {
-        return response.data(using: .utf8)!
+struct Trader2: ParsableCommand {
+    
+    enum Coin: EnumerableFlag {
+        case btc, eth
     }
-
-    init(response: String) {
-        self.response = response
-        parse()
+    
+    enum Action: EnumerableFlag {
+        case trade, record, simulate
     }
-
-    private func parse() {
-        let decoded = try! JSONDecoder().decode(MixedArray.self, from: responseData)
-        let responseArray = decoded.array
-//        let dataType = responseArray[2]
-        price = (responseArray[1] as! Prices).closePrice
-    }
-}
-
-struct MixedArray : Decodable {
-    var array = [Any]()
-
-    init(from decoder: Decoder) throws {
-        var arrayContainer = try decoder.unkeyedContainer()
-
-        while !arrayContainer.isAtEnd {
-            do {
-                array.append(String(try arrayContainer.decode(String.self)))
+    
+    @Flag(exclusivity: .chooseFirst, help: "The coin to trade.")
+    var coin: Coin = .btc
+    
+    @Flag(exclusivity: .chooseFirst, help: "What to do")
+    var action: Action = .record
+    
+    @Argument(help: "Where to save the file for recording.")
+    var recordFilePath: String?
+    
+    mutating func run() throws {
+        switch action {
+        case .record:
+            guard let filePath = recordFilePath else {
+                print("Giving a file path is mandatory for action 'record'")
+                Trader2.exit(withError: .some(ValidationError("Giving a file path is mandatory for action 'record'")))
             }
-            catch {}
-
-            do {
-                array.append(String(try arrayContainer.decode(Int.self)))
-            }
-            catch {
-
-            }
-
-            do {
-                array.append(try arrayContainer.decode(Prices.self))
-            }
-            catch {
-
-            }
+            
+            sourcePrint("Running trader with Binance api.")
+            let api = Kraken()
+            let trader = PriceRecorder(api: api, filePath: (filePath as NSString).expandingTildeInPath)
+            break
+        case .trade:
+            sourcePrint("Trading is not yet supported.")
+            break
+        case .simulate:
+            //print("Running trader with simulation api.")
+            //let api = SimulatedTradingPlatform(prices: prices)
+            //let trader = Trader(api: api)
+            //api.startSimulation(completed: {
+            //    print("Total profits: \(trader.profits)")
+            //})
+            break
         }
-    }
-
-    init() {}
-}
-
-
-struct IgnoreAsNil : Decodable {
-
-    init(from decoder: Decoder) throws {}
-}
-
-struct Prices: Decodable {
-
-    var c: MixedArray?
-
-    enum CodingKeys: String, CodingKey {
-        case c
-    }
-
-    var closePrice: Double {
-        return Double(c!.array[0] as! String)!
+        
     }
 }
+
+Trader2.main()
+RunLoop.main.run()
 
