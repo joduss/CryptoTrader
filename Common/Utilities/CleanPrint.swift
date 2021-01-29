a48a4348eded9a70e33c2244212a46de26e1cbd7
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-01-29 20:18:59 +0100

    Change the date in CleanPrint: it must use the DateFactory to have the right date during simulation.

--- Common/Utilities/CleanPrint.swift
+++ Common/Utilities/CleanPrint.swift
@@ -9,10 +9,10 @@ import Foundation
 
 /// Prints as if it was a log: [Date] [Class name] Message
 func sourcePrint(_ message: String, _ source: String = #file) {
-    print("[\(Date())] [\(source.fileName())] \(message)")
+    print("[\(DateFactory.now)] [\(source.fileName())] \(message)")
 }
 
 func sourceReplacablePrint(_ message: String, _ source: String = #file) {
-    print("\u{1B}[2K\u{1B}7[\(Date())] [\(source.fileName())] \(message) \u{1B}8", terminator: "")
+    print("\u{1B}[2K\u{1B}7[\(DateFactory.now)] [\(source.fileName())] \(message) \u{1B}8", terminator: "")
     fflush(stdout)
 }
