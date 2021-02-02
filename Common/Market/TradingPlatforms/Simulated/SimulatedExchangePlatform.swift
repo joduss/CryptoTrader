92478ddb988b4dcb97d2517c36f4380dbb845fa4
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-02-02 23:23:41 +0100

    Improve the simulated exchange platform with faster data deserialization.

--- Common/Market/TradingPlatforms/Simulated/SimulatedExchangePlatform.swift
+++ Common/Market/TradingPlatforms/Simulated/SimulatedExchangePlatform.swift
@@ -4,6 +4,9 @@ import JoLibrary
 public class SimulatedExchangePlatform: CryptoExchangePlatform {
     private let aggregatedTradesFilePath: String
     
+    private let semaphore = DispatchSemaphore(value: 1)
+    private let decoder = JSONDecoder()
+    
     public private(set) var subscribedToTickerStream: Bool = false
     public private(set) var subscribedToAggregatedTradeStream: Bool = false
     public private(set) var subscribedToMarketDepthStream: Bool = false
@@ -27,23 +30,51 @@ public class SimulatedExchangePlatform: CryptoExchangePlatform {
     
     public func subscribeToAggregatedTradeStream() {
         subscribedToAggregatedTradeStream = true
-                
-        let reader = TextFileReader.openFile(at: aggregatedTradesFilePath)
-                
+        
+        let reader = TextFileReader.openFile(at: self.aggregatedTradesFilePath)
+        
         while true {
             
             guard let line = reader.readLine() else {
+                (subscriber as? SimpleTrader)?.summary()
                 break
             }
             
-            let trade = try! JSONDecoder().decode(MarketAggregatedTrade.self, from: line.data(using: .utf8)!)
+            let splitLines = line.split(separator: ",")
             
-            DateFactory.simulated = true
-            DateFactory.now = trade.date
-            subscriber?.process(trade: trade)
+            let id = extractInt(startIndex: 6, in: splitLines[0])
+            let qty = extractDouble(startIndex: 11, in: splitLines[1])
+            let symbol = marketPair.rawValue
+            let price = extractDouble(startIndex: 8, in: splitLines[3])
+            let buyerIsMarker = true // don't care
+            var dateLine = splitLines[5]
+            dateLine.removeLast()
+            dateLine.removeLast()
+            let date = Date(timeIntervalSinceReferenceDate: extractDouble(startIndex: 7, in: dateLine))
+            
+            let trade = MarketAggregatedTrade(id: id, date: date, symbol: symbol, price: price, quantity: qty, buyerIsMaker: buyerIsMarker)
+            self.semaphore.wait()
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                DateFactory.simulated = true
+                DateFactory.now = trade.date
+                self.subscriber?.process(trade: trade)
+                self.semaphore.signal()
+            }
         }
-                
     }
+    
+    public func extractInt(startIndex: Int, in line: String.SubSequence) -> Int {
+        return Int(line[line.index(line.startIndex, offsetBy: startIndex)..<line.endIndex])!
+    }
+    
+    public func extractDouble(startIndex: Int, in line: String.SubSequence) -> Double {
+        return Double(line[line.index(line.startIndex, offsetBy: startIndex)..<line.endIndex])!
+    }
+    
+//    public func extractString(startIndex: Int, in line: String) -> String {
+//        return Int(line[line.index(line.startIndex, offsetBy: offsetBy)..<line.endIndex])!
+//    }
     
     public func subscribeToMarketDepthStream() {
         subscribedToMarketDepthStream = true
