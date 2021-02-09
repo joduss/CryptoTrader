04df0f85666ea042f3f4627a4a45410d5535255e
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-02-09 21:18:45 +0100

    Cleaned code of SimulatedExchangePlatform.

--- Common/Market/TradingPlatforms/Simulated/SimulatedExchangePlatform.swift
+++ Common/Market/TradingPlatforms/Simulated/SimulatedExchangePlatform.swift
@@ -1,32 +1,28 @@
 import Foundation
 import JoLibrary
 
+
 public class SimulatedExchangePlatform: CryptoExchangePlatform {
-    private let aggregatedTradesFilePath: String
     
-    private let semaphore = DispatchSemaphore(value: 1)
-    private let decoder = JSONDecoder()
-    
+    public let symbol: MarketPair
+    public var subscriber: CryptoExchangePlatformSubscriber?
+
     public private(set) var subscribedToTickerStream: Bool = false
     public private(set) var subscribedToAggregatedTradeStream: Bool = false
     public private(set) var subscribedToMarketDepthStream: Bool = false
     
     public private(set) var webSocketHandler: WebSocketHandler = WebSocketHandler(url: URL(string: "https://www.w.w")!)
     
-    public private(set) var marketPair: MarketPair
-    
-    public var subscriber: CryptoExchangePlatformSubscriber?
+    private let aggregatedTradesFilePath: String
+    private let semaphore = DispatchSemaphore(value: 1)
+    private let decoder = JSONDecoder()
     
     
     init(marketPair: MarketPair, aggregatedTradesFilePath: String) {
         self.aggregatedTradesFilePath = aggregatedTradesFilePath
-        self.marketPair = marketPair
+        self.symbol = marketPair
     }
     
-    
-    public func subscribeToTickerStream() {
-        subscribedToTickerStream = true
-    }
     
     public func subscribeToAggregatedTradeStream() {
         subscribedToAggregatedTradeStream = true
@@ -37,7 +33,7 @@ public class SimulatedExchangePlatform: CryptoExchangePlatform {
         
         while true {
             
-            guard var line = reader.readLine() else {
+            guard let line = reader.readLine() else {
                 (subscriber as? SimpleTrader)?.summary()
                 break
             }
@@ -52,7 +48,7 @@ public class SimulatedExchangePlatform: CryptoExchangePlatform {
             
             let id = extractInt(startIndex: 5, in: splitLines[1])
             let qty = extractDouble(startIndex: 10, in: splitLines[0])
-            let symbol = marketPair.rawValue
+            let symbolString = symbol.rawValue
             let price = extractDouble(startIndex: 8, in: splitLines[3])
             let buyerIsMarker = true // don't care
             var dateLine = splitLines[5]
@@ -60,7 +56,7 @@ public class SimulatedExchangePlatform: CryptoExchangePlatform {
             dateLine.removeLast()
             let date = Date(timeIntervalSinceReferenceDate: extractDouble(startIndex: 7, in: dateLine))
             
-            let trade = MarketAggregatedTrade(id: id, date: date, symbol: symbol, price: price, quantity: qty, buyerIsMaker: buyerIsMarker)
+            let trade = MarketAggregatedTrade(id: id, date: date, symbol: symbolString, price: price, quantity: qty, buyerIsMaker: buyerIsMarker)
             
             //            let trade = try! decoder.decode(MarketAggregatedTrade.self, from: line.data(using: .utf8)!)
             
@@ -76,23 +72,27 @@ public class SimulatedExchangePlatform: CryptoExchangePlatform {
         }
     }
     
-    public func extractInt(startIndex: Int, in line: String.SubSequence) -> Int {
+    // MARK: - Parsing helpers
+    
+    private func extractInt(startIndex: Int, in line: String.SubSequence) -> Int {
         return Int(line[line.index(line.startIndex, offsetBy: startIndex)..<line.endIndex])!
     }
     
-    public func extractDouble(startIndex: Int, in line: String.SubSequence) -> Double {
+    private func extractDouble(startIndex: Int, in line: String.SubSequence) -> Double {
         return Double(line[line.index(line.startIndex, offsetBy: startIndex)..<line.endIndex])!
     }
     
-//    public func extractString(startIndex: Int, in line: String) -> String {
-//        return Int(line[line.index(line.startIndex, offsetBy: offsetBy)..<line.endIndex])!
-//    }
+
+    // MARK: - Useless protocol implementation for the simulated exchange platform
     
     public func subscribeToMarketDepthStream() {
         subscribedToMarketDepthStream = true
     }
     
-    public func process(response: String) {
-        
-    } 
+    public func process(response: String) { }
+    
+    public func subscribeToTickerStream() {
+        subscribedToTickerStream = true
+    }
 }
+
