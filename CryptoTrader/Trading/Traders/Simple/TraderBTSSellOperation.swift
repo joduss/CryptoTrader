4b26c8123d7c5b0e653bc6637f46c99cb3edaae6
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-02-28 16:55:57 +0100

    Improve print messages.

--- CryptoTrader/Trading/Traders/Simple/TraderBTSSellOperation.swift
+++ CryptoTrader/Trading/Traders/Simple/TraderBTSSellOperation.swift
@@ -3,6 +3,8 @@ import Foundation
 
 class TraderBTSSellOperation: CustomStringConvertible, Codable {
     let uuid: String
+    let openDate: Date?
+    var closeDate: Date?
     
     let initialTrade: TraderBTSTrade
     
@@ -18,6 +20,7 @@ class TraderBTSSellOperation: CustomStringConvertible, Codable {
     
     
     init(trade: TraderBTSTrade) {
+        openDate = DateFactory.now
         uuid = UUID().uuidString.truncate(length: 5)
         self.initialTrade = trade
     }
@@ -27,6 +30,7 @@ class TraderBTSSellOperation: CustomStringConvertible, Codable {
         closingTrade = order
         
         profits = order.value - initialTrade.value
+        closeDate = DateFactory.now
     }
     
     var description: String {
@@ -35,26 +39,31 @@ class TraderBTSSellOperation: CustomStringConvertible, Codable {
     
     
     func description(currentPrice: Double) -> String {
+        let df = OutputDateFormatter.instance
+        
+        let openDateFormatted = openDate != nil ? "\(df.format(date: openDate!))" : "?"
+        
         switch status {
             case .new:
                 let currentValue = currentPrice * initialTrade.quantity
                 let originalValue = initialTrade.value
                 let diff = Percent(differenceOf: currentValue, from: originalValue).percentage
                 return
-                    "SellOperation \(uuid), Open. "
+                    "Open SellOperation \(uuid) since \(openDateFormatted). "
                     + "Value: original = \(originalValue.format(decimals: 3)), current = \(currentValue.format(decimals: 3)) (\(diff.format(decimals: 3))%). "
                     + "(\(initialTrade.price.format(decimals: 1)) -> \(currentPrice.format(decimals: 1)))"
             case .partiallyFilled:
-                return "SellOperation \(uuid), partially filled."
+                return "Partial-Fillet SellOperation \(uuid) since \(openDateFormatted)."
             case .filled:
-                let diff = Percent(differenceOf: closingTrade!.value, from: initialTrade.value).percentage
-                return "SellOperation \(uuid). Value: original = \(initialTrade.value), current = \(closingTrade!.value) (\(diff)%). (\(initialTrade.price) -> \(currentPrice))"
+                let diff = Percent(differenceOf: closingTrade!.value, from: initialTrade.value).percentage.format(decimals: 3)
+                var closeDateFormatted = closeDate != nil ? "\(df.format(date: closeDate!))" : "?"
+                return "Filled SellOperation \(uuid) the \(closeDateFormatted). Value: original = \(initialTrade.value.format(decimals: 3)), current = \(closingTrade!.value.format(decimals: 3)) (\(diff)%). (\(initialTrade.price.format(decimals: 2)) -> \(currentPrice.format(decimals: 2)))"
             case .cancelled:
-                return "SellOperation \(uuid) cancelled"
+                return "Cancelled SellOperation \(uuid)."
             case .rejected:
-                return "SellOperation \(uuid) rejected"
+                return "Rejected SellOperation \(uuid)."
             case .expired:
-                return "SellOperation \(uuid) expired"
+                return "Expired SellOperation \(uuid)."
         }
     }
 }
