2322d023fb7fa87ad235cbc306cd127d13706046
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-03-04 20:24:42 +0100

    Remove unused files.

--- CryptoTrader/Trading/Traders/Simulation/SimulationOrderState.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-
-enum SimulationOrderState {
-    case open // The order is open and ready to be closed
-    case closed // The order is finished
-    case limitLoss // The order is in mode limit loss, meaning we don't have the asset anymore. We sold it to buy it at a lower price.
-}
