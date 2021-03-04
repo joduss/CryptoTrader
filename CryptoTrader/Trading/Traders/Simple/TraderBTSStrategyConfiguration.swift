3387103f37e921c66b2321c9cff768cab330dcf0
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-03-04 20:34:11 +0100

    Create configuration with values found with grid search.

--- CryptoTrader/Trading/Traders/Simple/TraderBTSStrategyConfiguration.swift
+++ CryptoTrader/Trading/Traders/Simple/TraderBTSStrategyConfiguration.swift
@@ -3,37 +3,35 @@ import Foundation
 
 struct TraderBTSStrategyConfiguration {
     
-    var maxOrdersCount: Int = 9
+    var maxOrdersCount: Int = 10
     
     
     // Buying
 
     /// A stop-loss order is created with a price X% higher than the current price.
-    let buyStopLossPercent: Percent = 0.5
+    var buyStopLossPercent: Percent = 0.5 // 0.8
     
     /// Update the stop-loss order if the price goes lower than X% below the current price
-    let buyUpdateStopLossPercent: Percent = 0.7
+    var buyUpdateStopLossPercent: Percent = 0.01
         
-    let minDistancePercentNegative: Percent = -1.3
-    let minDistancePercentPositive: Percent = 0.71
+    var minDistancePercentNegative: Percent = -0.5 // 1.3 //(lower increases the revenue)
+    var minDistancePercentPositive: Percent = 0.5 // 0.5
+    
+    var nextBuyTargetPercent: Percent = 0.25 // negative
+    var nextBuyTargetExpiration: TimeInterval = TimeInterval.fromMinutes(60)
     
     
     // Selling
 
     /// The price increase(in % compared to current one) at which a stop-loss is created to
     /// take the profits if the price goes down again.
-    let sellStopLossProfitPercent: Percent = 0.7
-    
+    var sellStopLossProfitPercent: Percent = 1 // 0.8 (avec 0.2 => 14.31)
+    var minSellStopLossProfitPercent: Percent = 1
+
     /// Price increase (in % of the existing current price) at which the stop-loss profit
     /// price is updated.
-    let sellUpdateStopLossProfitPercent: Percent = 0.35
-    
-    // For very small profits.
-//    let sellLowerLimitDivisor = 4.0
+    var sellUpdateStopLossProfitPercent: Percent = 0.01
     
     /// Minimum lower limit compared to initial price at which we sell.
-    let sellMinProfitPercent: Percent = 0.25
-    
-    
-
+    var sellMinProfitPercent: Percent = 0.35
 }
