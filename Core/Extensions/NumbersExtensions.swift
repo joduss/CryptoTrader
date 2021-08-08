44721d5979e4bd948d84649079d7ce085b47b822
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-08-08 10:57:39 +0200

    Core: Create extension on numbers for formating with a single operator (£).

--- Core/Extensions/NumbersExtensions.swift
+++ Core/Extensions/NumbersExtensions.swift
@@ -1,11 +1,17 @@
 import Foundation
 
+infix operator £ : AdditionPrecedence
+
 extension Double {
     
     func format(decimals: Int) -> String {
         let formatter = NumberFormatter()
         formatter.maximumFractionDigits = decimals
         return formatter.string(from: self)!
+    }
+    
+    static func £(value: Double, decimals: Int) -> String {
+        return value.format(decimals: decimals)
     }
 }
 
@@ -23,6 +29,10 @@ extension Decimal {
     
     init?(_ string: String) {
         self.init(string: string)
+    }
+    
+    static func £(value: Decimal, decimals: Int) -> String {
+        return value.format(decimals: decimals)
     }
 }
 
